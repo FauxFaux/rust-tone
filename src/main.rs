435142ab336d5c51eaa664c17bc2eb218533b16f
Chris West (Faux) <git@goeswhere.com>
2018-06-08 11:50:03 +0100

    ui

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 extern crate cast;
+// libncursesw5-dev
+extern crate cursive;
 #[macro_use]
 extern crate failure;
 extern crate libpulse_binding as pulse;
@@ -9,23 +11,29 @@ use std::io::Write;
 
 use cast::f32;
 use cast::u8;
+use cursive::traits::Boxable;
+use cursive::traits::Identifiable;
+use cursive::views::Dialog;
+use cursive::views::EditView;
+use cursive::views::TextView;
 use failure::Error;
 use failure::ResultExt;
+use psimple::Simple;
+
+const RATE: u32 = 44100;
 
 fn main() -> Result<(), Error> {
     use psimple::Simple;
     use pulse::stream::Direction;
 
-    let rate = 44100;
-
     let spec = pulse::sample::Spec {
         format: pulse::sample::Format::U8,
         channels: 1,
-        rate,
+        rate: RATE,
     };
     ensure!(spec.is_valid(), "spec invalid!");
 
-    let s = Simple::new(
+    let simple = Simple::new(
         None,                // Use the default server
         "TunnelApp",         // Our application's name
         Direction::Playback, // We want a playback stream
@@ -37,17 +45,52 @@ fn main() -> Result<(), Error> {
     ).map_err(pulse)
         .with_context(|_| format_err!("connecting to server"))?;
 
-    let freq = 4000f32;
+    let mut siv = cursive::Cursive::new();
 
-    let buf = (0..rate)
-        .map(|i| u8(((f32(i) * freq / f32(rate)).cos() + 1.) * 127.))
+    siv.add_layer(
+        Dialog::new()
+            .title("Frequency (Hz)")
+            // Padding is (left, right, top, bottom)
+            .padding((1, 1, 1, 0))
+            .content(
+                EditView::new()
+                    .with_id("freq")
+                    .fixed_width(8),
+            )
+            .button("Play", move |s| {
+                // This will run the given closure, *ONLY* if a view with the
+                // correct type and the given ID is found.
+                let name = s.call_on_id("freq", |view: &mut EditView| {
+                    // We can return content from the closure!
+                    view.get_content()
+                }).unwrap();
+
+                // Run the next step
+                gui_tone(&simple, s, &name);
+            }),
+    );
+    siv.run();
+
+    Ok(())
+}
+
+fn gui_tone(s: &Simple, siv: &mut cursive::Cursive, val: &str) {
+    match val.parse() {
+        Ok(freq) => if let Err(e) = tone(s, freq) {
+            siv.add_layer(Dialog::info(format!("playback error: {:?}", e)));
+        },
+        Err(e) => siv.add_layer(Dialog::info(format!("parse error: {}", e))),
+    }
+}
+
+fn tone(s: &Simple, freq: f32) -> Result<(), Error> {
+    let buf = (0..RATE)
+        .map(|i| u8(((f32(i) * freq / f32(RATE)).cos() + 1.) * 127.))
         .collect::<Result<Vec<u8>, cast::Error>>()?;
 
     s.write(&buf)
         .map_err(pulse)
         .with_context(|_| format_err!("writing samples"))?;
-
-    fs::File::create("a.wav")?.write_all(&buf)?;
 
     Ok(())
 }
